3eeaea31626ecb4aae91add93e4cbb6a548dd736
dave6270 <dave6270@gmail.com>
2024-04-27 14:53:45 +0100

    Initial commit. Added brief comments

---
+++ Shell/depro3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Opens Disk Explorer Professional (a Windows program) in Linux using Wine.
+# Place in .local/shar/bin to run from the command line.
+
+cd "/home/david/.wine/drive_c/Program Files (x86)/Disk Explorer Professional 3/"
+wine depro.exe
+
